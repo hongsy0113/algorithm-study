53d28cf3f3f6fa5dc8f17a56787091b748d3cc31
SungYoon Hong <hsy0113@korea.ac.kr>
2022-03-08 18:55:55 +0900

    [UPDATE] 11047

--- baekjoon/greedy/11047/coin0.py
+++ baekjoon/greedy/11047/coin0.py
@@ -4,19 +4,28 @@ coin_list = []
 count = 0
 
 for i in range(n):
-    coin_list.append(int(input()))
+    value = int(input())
+    if value <= k:
+        coin_list.append(value)
 
 coin_list.reverse()
 
-i = 0
-while True:
-    coin = coin_list[i]
-    if coin <= k:
-        k -= coin
-        count += 1
+## 시간초과
+# i = 0
+# while True:
+#     coin = coin_list[i]
+#     if coin <= k:
+#         k -= coin
+#         count += 1
+#     else:
+#         i += 1
+#     if k <= 0 :
+#         break
+for i in coin_list:
+    if k >= i:
+        num = k // i
+        k -= i * num
+        count += num
     else:
-        i += 1
-    if k <= 0 :
-        break
-
+        continue
 print(count)
